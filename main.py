ad086446bd68b13ecf6be2b17d2740d83739cf06
matthewstephenjones-debug <matthew_stephen_jones@hotmail.com>
2026-08-15 14:23:44 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,13 +3,8 @@ import requests
 
 BOT_TOKEN = os.environ["TELEGRAM_TOKEN"]
 CHAT_ID = os.environ["CHAT_ID"]
-API_KEY = os.environ["FINNHUB_API_KEY"]
 
-url = f"https://finnhub.io/api/v1/stock/symbol?exchange=US&token={API_KEY}"
-
-r = requests.get(url)
-
-message = str(r.json())[:3500]
+message = "✅ Finnhub key loaded successfully"
 
 requests.post(
     f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage",
@@ -18,4 +13,3 @@ requests.post(
         "text": message
     }
 )
-
